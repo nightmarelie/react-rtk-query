367899d6453196f8ed4364b7f8d1a15a1a282ac1
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-09-16 20:23:10 -0600

    fix: tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <h1 className="text-5xl font-bold uppercase underline">Hello world!</h1>
+  );
 }
 
 export default App;
